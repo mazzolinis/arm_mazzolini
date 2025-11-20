0347aa22279280aafcfe8a1c4f8eec9cdbe1b3da
mazzolinis <mazzolini.sim@gmail.com>
2025-11-20 23:08:42 +0100

    almost ready for testing

--- include/arm_mazzolini/target_spawner.hpp
+++ include/arm_mazzolini/target_spawner.hpp
@@ -2,7 +2,6 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/point_stamped.hpp>
-#include <geometry_msgs/msg/point_stamped.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <std_msgs/msg/bool.hpp>
 
@@ -14,4 +13,21 @@ class TargetSpawner : public rclcpp::Node
 {
     public:
         TargetSpawner();
+
+    private:
+        // Publisher
+        rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr target_pub;
+
+        // Subscriber
+        rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr laser_sub;
+
+        // Timer and buffer
+        rclcpp::TimerBase::SharedPtr timer;
+        std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+        std::shared_ptr<tf2_ros::TransformListener> tf_listener;
+
+        // Callbacks
+        void timer_callback();
+        void laser_Callback(const std_msgs::msg::Bool::SharedPtr msg);
+        void pose_callback(const geometry_msgs::msg::TransformStamped::SharedPtr msg);
 }
--- src/target_spawner.cpp
+++ src/target_spawner.cpp
@@ -2,5 +2,12 @@
 
 TargetSpawner::TargetSpawner() : Node("target_spawner_node")
 {
-    
+    target_pub = this->create_publisher<geometry_msgs::msg::PointStamped>("/target_position", 10);
+    laser_sub = this->create_subscription<std_msgs::msg::Bool>(
+        "/laser_command", 10,
+        std::bind(&TargetSpawner::laser_Callback, this, std_placeholders::_1)
+    );
+    tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+    timer = this->create_wall_timer(std::chrono::seconds(spawn_period), std::bind(&TargetSpawner::timer_callback, this));
 }
